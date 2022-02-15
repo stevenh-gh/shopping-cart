3def2c9f502881f780264a369e3c7f91271e797b
steven <steven.h@live.com>
2022-02-15 13:52:28 -0800

    remove useEffect since RouteSwitch will use it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import { useEffect } from "react";
-
 const App = () => {
-    useEffect(() => {
-        document.getElementById("root").classList.add("container", "m-auto");
-    }, []);
-    return <h1 className="text-3xl">react app</h1>;
+    return <h1 className="text-3xl">this is the homepage</h1>;
 };
 
 export default App;
