b500248656cf61fd7a08977e1b76650226a8423c
steven <steven.h@live.com>
2022-02-15 13:58:03 -0800

    add nabigation button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 const App = () => {
-    return <h1 className="text-3xl">this is the homepage</h1>;
+    return (
+        <>
+            <h1 className="text-3xl">this is the homepage</h1>
+            <button className="border-2 border-black rounded-md">
+                <a href="/shop">to shopping page</a>
+            </button>
+        </>
+    );
 };
 
 export default App;
--- src/Shop.js
+++ src/Shop.js
@@ -1,5 +1,12 @@
 const Shop = () => {
-    return <h1 className="text-3xl">this is the shopping page</h1>;
+    return (
+        <>
+            <h1 className="text-3xl">this is the shopping page</h1>
+            <button className="border-2 border-black rounded-md">
+                <a href="/">to homepage</a>
+            </button>
+        </>
+    );
 };
 
 export default Shop;
