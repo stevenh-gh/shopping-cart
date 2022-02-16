049375a617d961d58a0fb1f6f26ba57c8b70116e
steven <steven.h@live.com>
2022-02-16 14:43:39 -0800

    replace header with navbar

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,14 @@
 const Shop = () => {
     return (
         <>
+            <nav className="flex justify-end space-x-10 pt-7">
+                <a href="/">home</a>
+                <a href="/shop">shop</a>
             <h1 className="text-3xl">this is the shopping page</h1>
             <button className="border-2 border-black rounded-md">
                 <a href="/">to homepage</a>
             </button>
+            </nav>
         </>
     );
 };
