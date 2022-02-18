db887a40dba5907063c512eeae347a2e59e00f9e
steven <steven.h@live.com>
2022-02-17 22:52:10 -0800

    display quantity in cart area

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import Card from "./Card";
+import { useState } from "react";
 
 const Shop = () => {
     const itemList = [
@@ -24,6 +25,10 @@ const Shop = () => {
         setCart(cart.concat(item));
     };
 
+    const itemsInCart = () => {
+        return cart.reduce((acc, currentVal) => acc + currentVal.quantity, 0);
+    };
+
     return (
         <>
             <nav className="flex justify-end space-x-10 pt-7">
@@ -32,6 +37,10 @@ const Shop = () => {
                 <div className="mr-2">
                     {/* todo: add link to checkout */}
                     <FontAwesomeIcon icon={faCartShopping} />
+                    {/* todo: if item quanitity is 1, then item else items */}
+                    <div className="pl-2 inline-block">
+                        ({itemsInCart()} items)
+                    </div>
                 </div>
             </nav>
             <Card item={itemList[0]} data={0} submit={handleSubmit} />
