0aaddab6960499e682df081d102c03247e0daee1
steven <steven.h@live.com>
2022-02-17 22:51:46 -0800

    add data-attribute to Card

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 const Card = (props) => {
     const { item, submit, data } = props;
     return (
-        <div className="flex border border-black w-fit">
+        <div data-index={data} className="flex border border-black w-fit">
             {/* todo: img src, name, description, etc will be replaced with props */}
             <img src={item.picUrl} alt="" />
             <div className="pl-2 pr-2">
