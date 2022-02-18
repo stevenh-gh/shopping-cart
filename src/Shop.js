c276968c879c7655cd5a96004a5a817948764d60
steven <steven.h@live.com>
2022-02-17 22:50:54 -0800

    add state for cart and a handleSubmit function which will be passed down as prop

--- src/Shop.js
+++ src/Shop.js
@@ -8,6 +8,22 @@ const Shop = () => {
         { title: "item 2", price: 11.99, picUrl: "https://picsum.photos/200" },
         { title: "item 3", price: 12.99, picUrl: "https://picsum.photos/200" },
     ];
+
+    const [cart, setCart] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // const dataIndex = Number(e.nativeEvent.path[2].attributes[0].value);
+        // const quantity = Number(e.target[0].value);
+        // todo: if quantity is 0, do nothing
+        const item = Object.assign(
+            {},
+            itemList[Number(e.nativeEvent.path[2].attributes[0].value)]
+        );
+        item.quantity = Number(e.target[0].value);
+        setCart(cart.concat(item));
+    };
+
     return (
         <>
             <nav className="flex justify-end space-x-10 pt-7">
