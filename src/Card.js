ea50a233fea19f133286bb2acb4adee2e3b2698d
steven <steven.h@live.com>
2022-02-17 22:41:48 -0800

    replace items info with props

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,24 @@
-const Card = () => {
+const Card = (props) => {
+    const { item, submit, data } = props;
     return (
         <div className="flex border border-black w-fit">
             {/* todo: img src, name, description, etc will be replaced with props */}
-            <img src="https://picsum.photos/200" alt="" />
-            <div>
-                <div>item name</div>
-                <div>description</div>
-                <form>
-                    <input type="number" name="" id="" min={0} value={0} />
+            <img src={item.picUrl} alt="" />
+            <div className="pl-2 pr-2">
+                <div>{item.title}</div>
+                <form onSubmit={submit}>
+                    <input
+                        type="number"
+                        name="inputname"
+                        id="inputid"
+                        min={0}
+                        placeholder={0}
+                    />
                     <br />
                     {/* todo: style input button */}
                     <input type="submit" value="Add to cart" />
                 </form>
+                <div>${item.price}</div>
             </div>
         </div>
     );
