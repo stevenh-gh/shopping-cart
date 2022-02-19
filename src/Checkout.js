f9602cb929ba87690e5585e28837803d043f02a2
steven <steven.h@live.com>
2022-02-18 17:00:32 -0800

    console log cart state

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 const Checkout = () => {
+    const location = useLocation();
+    const { cart } = location.state;
+    console.log(cart);
     return (
         <div>
             <div>in checkout</div>
