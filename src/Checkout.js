6624d58da3f1e10b891a63ae781157691e3bdc15
steven <steven.h@live.com>
2022-02-20 16:05:36 -0800

    add extra column for "price"

--- src/Checkout.js
+++ src/Checkout.js
@@ -8,29 +8,27 @@ const Checkout = () => {
         <div>
             <div>in checkout</div>
             <div className="border border-black">
-                <div className="grid grid-cols-3 font-bold">
+                <div className="grid grid-cols-4 font-bold">
                     <div>title</div>
+                    <div>price</div>
                     <div>quantity</div>
                     <div>total</div>
                 </div>
                 <hr className="h-3" />
                 {Object.values(cart).map((val, index) => {
                     return (
-                        <div key={index} className="grid grid-cols-3">
+                        <div key={index} className="grid grid-cols-4">
                             <div>{val.title}</div>
+                            <div>{val.price}</div>
                             <div>{val.quantity}</div>
                             <div>{val.total}</div>
                         </div>
                     );
                 })}
             </div>
-            <div className="grid grid-cols-3">
-                <div className="font-bold col-start-2">grand total:</div>
-                <div className="col-start-3">
-                    {Object.values(cart).reduce(
-                        (acc, curr) => acc + curr.total,
-                        0
-                    )}
+            <div className="grid grid-cols-4">
+                <div className="font-bold col-start-3">grand total:</div>
+                <div className="col-start-4">
                 </div>
             </div>
         </div>
