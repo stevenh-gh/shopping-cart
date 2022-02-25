30f4d9835acf821a44e686a5e377c19af4b1a78c
steven <steven.h@live.com>
2022-02-25 13:26:41 -0800

    remove props from Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,11 @@
 import Cart from "./Cart";
 
-const Nav = ({ cart, itemsInCart }) => {
+const Nav = () => {
     return (
         <nav className="flex justify-end space-x-10 pt-7">
             <a href="/">home</a>
             <a href="/shop">shop</a>
-            <Cart cart={cart} itemsInCart={itemsInCart} />
+            <Cart />
         </nav>
     );
 };
