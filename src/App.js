63bccd79fc714019985647da64a758e4d71e2c1c
steven <steven.h@live.com>
2022-02-25 13:28:12 -0800

    add Nav to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Nav from "./Nav";
+
 const App = () => {
     return (
         <>
+            <Nav />
             <h1 className="text-3xl">this is the homepage</h1>
             <button className="border-2 border-black rounded-md">
                 <a href="/shop">to shopping page</a>
