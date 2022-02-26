9695296b8a32cb88253376744455ff56a48bed32
steven <steven.h@live.com>
2022-02-26 15:53:45 -0800

    - add function to handle item deletion - add checkout state - replace json parse in jsx with state

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,22 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import Nav from "./Nav";
 
 const Checkout = () => {
+    const [checkout, setCheckout] = useState(
+        JSON.parse(localStorage.getItem("cart")) || []
+    );
+
+    const handleDelete = (e) => {
+        const index = e.currentTarget.dataset.index;
+        const cart = JSON.parse(localStorage.getItem("cart"));
+        cart.splice(index, 1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        window.dispatchEvent(new Event("storage"));
+        setCheckout(cart);
+    };
+
     return (
         <div>
             <Nav />
@@ -15,29 +29,29 @@ const Checkout = () => {
                     <div>total</div>
                 </div>
                 <hr className="h-3" />
-                {Object.values(JSON.parse(localStorage.getItem("cart"))).map(
-                    (val, index) => {
-                        return (
-                            <div key={index} className="grid grid-cols-4">
-                                <div className="group">
-                                    {val.title}
-                                    <FontAwesomeIcon
-                                        icon={faXmark}
-                                        className="ml-4 text-red-600 invisible group-hover:visible hover:cursor-pointer"
-                                    />
-                                </div>
-                                <div>{val.price}</div>
-                                <div>{val.quantity}</div>
-                                <div>{val.total.toFixed(2)}</div>
+                {Object.values(checkout).map((val, index) => {
+                    return (
+                        <div key={index} className="grid grid-cols-4">
+                            <div className="group">
+                                {val.title}
+                                <FontAwesomeIcon
+                                    icon={faXmark}
+                                    className="ml-4 text-red-600 invisible group-hover:visible hover:cursor-pointer"
+                                    onClick={handleDelete}
+                                    data-index={index}
+                                />
                             </div>
-                        );
-                    }
-                )}
+                            <div>{val.price}</div>
+                            <div>{val.quantity}</div>
+                            <div>{val.total.toFixed(2)}</div>
+                        </div>
+                    );
+                })}
             </div>
             <div className="grid grid-cols-4 mt-5">
                 <div className="font-bold col-start-3">grand total:</div>
                 <div className="col-start-4">
-                    {Object.values(JSON.parse(localStorage.getItem("cart")))
+                    {Object.values(checkout)
                         .reduce((acc, curr) => acc + curr.total, 0)
                         .toFixed(2)}
                 </div>
