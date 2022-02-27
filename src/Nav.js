a087d4a7977ec47ae1a72e2f59e2a501878e43e0
steven <steven.h@live.com>
2022-02-27 13:12:10 -0800

    add shop name and a bit more styles

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 import Cart from "./Cart";
 
 const Nav = () => {
     return (
-        <nav className="flex justify-end space-x-10 pt-7">
-            <a href="/">home</a>
-            <a href="/shop">shop</a>
-            <Cart />
+        <nav className="flex justify-between mt-2 mb-3">
+            <Link to="/" className="text-3xl">
+                Keyboards Town
+            </Link>
+            <div className="flex justify-end space-x-10 pt-7">
+                <a href="/">home</a>
+                <a href="/shop">shop</a>
+                <Cart />
+            </div>
         </nav>
     );
 };
