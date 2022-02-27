0edc7ee2a473f6d45e61a825c2efa173c829bb0b
steven <steven.h@live.com>
2022-02-27 13:15:22 -0800

    - add row gaps to grid - add dollar symbol to prices

--- src/Checkout.js
+++ src/Checkout.js
@@ -20,18 +20,17 @@ const Checkout = () => {
     return (
         <div>
             <Nav />
-            <div>in checkout</div>
             <div className="border border-black">
-                <div className="grid grid-cols-4 font-bold">
-                    <div>title</div>
-                    <div>price</div>
-                    <div>quantity</div>
-                    <div>total</div>
+                <div className="grid grid-cols-4 font-bold gap-x-4">
+                    <div>Item</div>
+                    <div>Price</div>
+                    <div>Quantity</div>
+                    <div>Subtotal</div>
                 </div>
                 <hr className="h-3" />
                 {Object.values(checkout).map((val, index) => {
                     return (
-                        <div key={index} className="grid grid-cols-4">
+                        <div key={index} className="grid grid-cols-4 gap-x-4">
                             <div className="group">
                                 {val.title}
                                 <FontAwesomeIcon
@@ -41,16 +40,17 @@ const Checkout = () => {
                                     data-index={index}
                                 />
                             </div>
-                            <div>{val.price}</div>
+                            <div>${val.price}</div>
                             <div>{val.quantity}</div>
-                            <div>{val.total.toFixed(2)}</div>
+                            <div>${val.total.toFixed(2)}</div>
                         </div>
                     );
                 })}
             </div>
-            <div className="grid grid-cols-4 mt-5">
-                <div className="font-bold col-start-3">grand total:</div>
+            <div className="grid grid-cols-4 mt-5 gap-x-4">
+                <div className="font-bold col-start-3">Grand total:</div>
                 <div className="col-start-4">
+                    $
                     {Object.values(checkout)
                         .reduce((acc, curr) => acc + curr.total, 0)
                         .toFixed(2)}
