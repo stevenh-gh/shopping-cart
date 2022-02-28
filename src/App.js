81394643b9ac390c874e6e06ff3c3b756b7fe134
steven <steven.h@live.com>
2022-02-28 14:41:29 -0800

    style the front page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
 const App = () => {
     return (
         <>
             <Nav />
-            <h1 className="text-3xl">this is the homepage</h1>
-            <button className="border-2 border-black rounded-md">
-                <a href="/shop">to shopping page</a>
-            </button>
+            <div className="grid grid-cols-12 grid-rows-2 gap-1">
+                <h1 className="text-3xl col-span-4 col-start-5">
+                    Welcome to Keyboards Town!
+                </h1>
+                <Link
+                    className="bg-blue-400 p-4 text-white rounded-md hover:bg-blue-300 row-start-2 col-span-5 col-start-4 text-center mt-2"
+                    to="/shop"
+                >
+                    Click here to begin shopping!
+                    <FontAwesomeIcon icon={faCartShopping} className="ml-3" />
+                </Link>
+            </div>
         </>
     );
 };
