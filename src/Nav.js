41c6b64e749708beea1886c80c6468079354a4a6
steven <steven.h@live.com>
2022-03-01 14:36:19 -0800

    change anchor element to Link component

--- src/Nav.js
+++ src/Nav.js
@@ -8,8 +8,8 @@ const Nav = () => {
                 Keyboards Town
             </Link>
             <div className="flex justify-end space-x-10 pt-7">
-                <a href="/shopping-cart">home</a>
-                <a href="/shopping-cart/shop">shop</a>
+                <Link to="/shopping-cart">home</Link>
+                <Link to="/shopping-cart/shop">shop</Link>
                 <Cart />
             </div>
         </nav>
