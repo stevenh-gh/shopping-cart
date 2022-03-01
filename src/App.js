77be491dc06b528243426c760858a332b14e2c1c
steven <steven.h@live.com>
2022-02-28 21:12:51 -0800

    style/position the image

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     return (
         <>
             <Nav />
-            <div className="grid grid-cols-12 grid-rows-2 gap-1">
+            <div className="grid grid-cols-12 grid-rows-[repeat(4, auto)] gap-1">
                 <h1 className="text-3xl col-span-4 col-start-5">
                     Welcome to Keyboards Town!
                 </h1>
@@ -18,20 +18,20 @@ const App = () => {
                     Click here to begin shopping!
                     <FontAwesomeIcon icon={faCartShopping} className="ml-3" />
                 </Link>
+                <figure className="row-start-3 col-span-5 col-start-4 m-auto">
+                    <img src={require("./bg.jpg")} className="h-96" alt="" />
+                    <figcaption className="text-sm">
+                        Photo by{" "}
+                        <a href="https://unsplash.com/@nuclear?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Bernin Uben
+                        </a>{" "}
+                        on{" "}
+                        <a href="https://unsplash.com/s/photos/keyboards?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                            Unsplash
+                        </a>
+                    </figcaption>
+                </figure>
             </div>
-            <figure>
-                <img src={require("./bg.jpg")} className="h-96" alt="" />
-                <figcaption className="text-sm">
-                    Photo by{" "}
-                    <a href="https://unsplash.com/@nuclear?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                        Bernin Uben
-                    </a>{" "}
-                    on{" "}
-                    <a href="https://unsplash.com/s/photos/keyboards?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-                        Unsplash
-                    </a>
-                </figcaption>
-            </figure>
         </>
     );
 };
